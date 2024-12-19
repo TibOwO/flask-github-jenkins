459d4a8d3de1df3945e8d4b689248be4ce5ef991
Tibo <thibaultamettler@gmail.com>
2024-12-19 11:53:43 +0100

    rohhh

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 import pytest
-from .. import app
+#from "../" import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
